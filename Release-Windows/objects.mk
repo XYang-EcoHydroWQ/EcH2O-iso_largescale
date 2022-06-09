1b65bab72e0e6f0c7be0487776c05ed5e7828778
Xiaoqiang Yang <xiaoqiang.yang@ufz.de>
2022-06-09 13:28:25 +0200

    Delete unused compile files

--- Release-Windows/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibcsf64 -lboost_serialization.dll -larmadillo.dll
-
